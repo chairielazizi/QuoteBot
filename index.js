05c3685937d2bc9062db32281b01cff9b99656ec
chairielazizi <azizihr2000@gmail.com>
2020-12-21 14:49:29 +0800

    basic setup

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-require('dotenv').config();
+// require('dotenv').config();
+// export const token = process.env[token];
 const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require("./config.json");
 
-export const token = process.env[token];
+client.on('ready',() => {
+    console.log("The client is ready");
+})
+
+client.login(config.token);
