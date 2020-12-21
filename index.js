1a3698e5c89c1385d1ab443e364b907871861001
chairielazizi <azizihr2000@gmail.com>
2020-12-21 14:40:12 +0800

    problem

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 
-// const token = "NzkwMjU2NTA2MjQyOTkwMDkw.X999uQ.-xuM_nCyeOD2ZcRmC5fyAYouBtc";
 export const token = process.env[token];
