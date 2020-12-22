f1e958f19511cf4632292989449029c6f2142f37
chairielazizi <azizihr2000@gmail.com>
2020-12-22 11:36:06 +0800

    basic command handler code

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 // export const token = process.env[token];
 const Discord = require('discord.js');
 const client = new Discord.Client();
-// const config = require("./config.json");
+const config = require("./config.json");
+const command = require('./command');
 
 client.on('ready',() => {
     console.log("The client is ready");
+
+    command(client, ['ping','test'], (message) => {
+        message.channel.send('Pongo!');
+    })
 })
 
-// client.login(config.token);
+client.login(config.token);
 client.login(process.env.token);
