0f46c94eb9c5298bc3561a352cafc69389740a3b
chairielazizi <azizihr2000@gmail.com>
2021-02-02 12:34:03 +0800

    use fetch api instead of xmlhttprequest

--- first-message.js
+++ first-message.js
@@ -1,27 +1,27 @@
-const addReactions = (message, reactions) =>{
-    message.react(reactions[0])
-    reactions.shift() // move the 0 index to the left
-    if(reactions.length > 0){
-        setTimeout(() => addReactions(message,reactions), 750) //milisecond
+const addReactions = (message, reactions) => {
+  message.react(reactions[0]);
+  reactions.shift(); // move the 0 index to the left
+  if (reactions.length > 0) {
+    setTimeout(() => addReactions(message, reactions), 750); //milisecond
+  }
+};
+
+module.exports = async (client, id, text, reactions = []) => {
+  const channel = await client.channels.fetch(id);
+
+  channel.messages.fetch().then((messages) => {
+    if (messages.size === 0) {
+      // send a new message
+      channel.send(text).then((message) => {
+        addReactions(message, reactions);
+      });
+    } else {
+      // edit existing message
+      for (const message of messages) {
+        console.log(message);
+        // message[1].edit(text)
+        // addReactions(message[1], reactions)
+      }
     }
-}
-
-module.exports = async(client,id,text,reactions = []) => {
-    const channel = await client.channels.fetch(id)
-
-    channel.messages.fetch().then((messages) => {
-        if(messages.size === 0) {
-            // send a new message
-            channel.send(text).then(message => {
-                addReactions(message,reactions)
-            })
-        } else{
-            // edit existing message
-            for (const message of messages) {
-                console.log(message);
-                // message[1].edit(text)
-                // addReactions(message[1], reactions)
-            }
-        }
-    })
-}
+  });
+};
--- index.js
+++ index.js
@@ -1,86 +1,120 @@
 // require('dotenv').config();
 // export const token = process.env[token];
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
+const fetch = require("node-fetch");
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var request = new XMLHttpRequest()
+var request = new XMLHttpRequest();
 
-function getQuote(){
-    var quote;
-    request.open('GET','https://zenquotes.io/api/random',true)
-    request.onload = function(){
-        // begin acessing JSON data here
-        var data = JSON.parse(this.responseText);
-        quote = data[0]['q'] + " -" + data[0]['a']
-        
+function getQuote() {
+  var quote;
+  request.open("GET", "https://zenquotes.io/api/random", true);
+  request.responseType = "json";
+  request.onload = function () {
+    // begin acessing JSON data here
+    var status = request.status;
+    if (status == 200) {
+      console.log(request.response);
+    } else {
+      console.log(error);
     }
-    request.send()
-    return quote;
+    var data = JSON.parse(this.responseText);
+    // var data = request.response;
+    console.log(data);
+    quote = data[0].q + data[0].a;
+    // quote = JSONStringify(data.q) + " -" + JSONStringify(data.a)
+    request.send();
+  };
+
+  return quote;
 }
 
+fetch("https://zenquotes.io/api/random")
+  .then((res) => res.json())
+  .then((data) => {
+    // data = res[0].q + res[0].a;
+    // data = JSON.parse(this.response);
+    console.log(data);
+  })
+  .catch((err) => console.log(err));
+
+const url = "https://zenquotes.io/api/random";
+async function get(url) {
+  // Storing response
+  const response = await fetch(url);
+
+  // Storing data in form of JSON
+  var data = await response.json();
+  // console.log(data);
+  var output = data[0].q + " -" + data[0].a + "-";
+  console.log(output);
+  return output;
+}
 
 const config = require("./config.json");
-const command = require('./command');
-const firstMessage = require("./first-message")
+const command = require("./command");
+// const firstMessage = require("./first-message");
 const privateMessage = require("./private-message");
 
-client.on('ready',() => {
-    console.log("The client is ready");
+client.on("ready", async () => {
+  console.log("The client is ready");
 
-    command(client,['q','quote'], message => {
-        quote = getQuote()
-        message.channel.send(quote);
-    })
+  command(client, ["q", "quote"], async (message) => {
+    // quote = getQuote();
+    quote = await get(url);
+    // await console.log(quote);
+    await message.channel.send(quote);
+  });
 
-    command(client, ['ping','test'], (message) => {
-        message.channel.send('Pongo!');
-    })
+  command(client, ["ping", "test"], (message) => {
+    message.channel.send("Pongo!");
+  });
 
-    // list the members of the server
-    command(client,'servers',message=>{
-        client.guilds.cache.forEach((guild) => {
-            console.log(guild);
-            message.channel.send(`${guild.name} has a total of ${guild.memberCount} members`);
-        })
-    })
+  // list the members of the server
+  command(client, "servers", (message) => {
+    client.guilds.cache.forEach((guild) => {
+      console.log(guild);
+      message.channel.send(
+        `${guild.name} has a total of ${guild.memberCount} members`
+      );
+    });
+  });
 
-    // delete recent messages on the channel
-    command(client,['cc','clear'],(message) => {
-        if(message.member.hasPermission('ADMINISTRATOR')) {
-            message.channel.messages.fetch().then((results) => {
-                console.log(results); // get all the messages
-                // for (const result of results){
-                //     message.result.delete()
-                // }
-                // message.channel.bulkDelete(results);
-            })
-        } 
-    })
+  // delete recent messages on the channel
+  command(client, ["cc", "clear"], (message) => {
+    if (message.member.hasPermission("ADMINISTRATOR")) {
+      message.channel.messages.fetch().then((results) => {
+        console.log(results); // get all the messages
+        // for (const result of results){
+        //     message.result.delete()
+        // }
+        message.channel.bulkDelete(results);
+      });
+    }
+  });
 
-    // set the status of the bot
-    command(client,'status',message => {
-        const content = message.content.replace('!status','');
+  // set the status of the bot
+  command(client, "status", (message) => {
+    const content = message.content.replace("!status", "");
 
-        // "!status hello world" -> "hello world"
-        client.user.setPresence({
-            activity:{
-                name: content,
-                type: 0,
-            },
-        })
-    })
+    // "!status hello world" -> "hello world"
+    client.user.setPresence({
+      activity: {
+        name: content,
+        type: 0,
+      },
+    });
+  });
 
+  // firstMessage(client,'791078758572490763','halu',['😍','😁','🐱‍🐉','🎉'])
+  // firstMessage(client,'782247082915921944','hohoho',['😍','😁','🐱‍🐉','🎉'])
 
-    // firstMessage(client,'791078758572490763','halu',['😍','😁','🐱‍🐉','🎉'])
-    // firstMessage(client,'782247082915921944','hohoho',['😍','😁','🐱‍🐉','🎉'])
-
-    privateMessage(client,"ping","pong");
-    client.users.fetch('464470476391972874').then(user => {
-        user.send('Ahoy World!');
-    })
-
-})
+  privateMessage(client, "ping", "pong");
+  client.users.fetch("464470476391972874").then((user) => {
+    user.send("Ahoy World!");
+  });
+});
 
 client.login(config.token);
 // client.login(process.env.token);
